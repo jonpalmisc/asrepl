54c97d350d5aca56f9ace552b707ca23f6d5707d
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-10 12:43:56 -0400

    Add C++ linkage safety guards

--- core/prompt.h
+++ core/prompt.h
@@ -6,5 +6,13 @@ typedef struct {
     asrepl_engine* engine;
 } asrepl_prompt;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int asrepl_prompt_init(asrepl_prompt* p);
 char* asrepl_prompt_send(asrepl_prompt* p, const char* input);
+
+#ifdef __cplusplus
+}
+#endif
