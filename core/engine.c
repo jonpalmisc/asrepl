b36e740ec49798899c528299f9b376e8f37a2c15
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-10 17:31:38 -0400

    Use 64-bit x86 by default, misc. core cleanup

--- core/engine.c
+++ core/engine.c
@@ -6,7 +6,7 @@ int asrepl_engine_init(asrepl_engine* e)
 {
     ks_engine* ks;
 
-    ks_err err = ks_open(KS_ARCH_X86, KS_MODE_32, &ks);
+    ks_err err = ks_open(KS_ARCH_X86, KS_MODE_64, &ks);
     if (err != KS_ERR_OK)
         return 1;
 
@@ -16,20 +16,18 @@ int asrepl_engine_init(asrepl_engine* e)
 
 int asrepl_engine_asm(asrepl_engine* e, const char* input, char** out)
 {
-    ks_err err;
     size_t count;
-    unsigned char* encode;
+    unsigned char* code;
     size_t size;
 
-    if (ks_asm(e->keystone, input, 0, &encode, &size, &count) != KS_ERR_OK)
+    if (ks_asm(e->keystone, input, 0, &code, &size, &count) != KS_ERR_OK)
         return 1;
 
-    size_t i;
     char* result = calloc(size + 1, sizeof(char));
     for (size_t i = 0; i < size; i++)
-        sprintf(&result[i * 2], "%02x", (int)encode[i]);
+        sprintf(&result[i * 2], "%02x", (int)code[i]);
 
-    ks_free(encode);
+    ks_free(code);
     *out = result;
     return 0;
 }
