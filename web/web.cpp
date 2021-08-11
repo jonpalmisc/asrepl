6a63e4b85c3eb92d160c7d6eaab3c2621acc1393
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-11 18:40:17 -0400

    Fix web target after rewrite

--- web/web.cpp
+++ web/web.cpp
@@ -3,26 +3,14 @@
 #include <emscripten/bind.h>
 #include <emscripten/emscripten.h>
 
-#include <string>
+static asrepl::prompt g_prompt;
 
-static asrepl_prompt* g_prompt;
-
-EMSCRIPTEN_KEEPALIVE void asrepl_web_init()
+EMSCRIPTEN_KEEPALIVE std::string send(const std::string& input)
 {
-    asrepl_prompt_init(g_prompt);
-}
-
-EMSCRIPTEN_KEEPALIVE std::string asrepl_web_send(const std::string& input)
-{
-    auto* raw_result = asrepl_prompt_send(g_prompt, input.c_str());
-    std::string result(raw_result);
-
-    free(raw_result);
-    return result;
+    return g_prompt.send(input);
 }
 
 EMSCRIPTEN_BINDINGS(asrepl_web)
 {
-    emscripten::function("init", &asrepl_web_init);
-    emscripten::function("send", &asrepl_web_send);
+    emscripten::function("asreplSend", &send);
 }
