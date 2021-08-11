74d58d56d9f5eb75d1584a515933f34a65a90777
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-10 23:44:39 -0400

    Actually make Capstone work

--- core/engine.c
+++ core/engine.c
@@ -11,10 +11,8 @@ int asrepl_engine_init(asrepl_engine* e)
         return 1;
     e->keystone = ks;
 
-    if (cs_open(CS_ARCH_X86, CS_MODE_32, &e->capstone) != CS_ERR_OK) {
-        printf("asrepl_core: Failed to initialize Capstone\n");
+    if (cs_open(CS_ARCH_X86, CS_MODE_64, &e->capstone) != CS_ERR_OK)
         return 1;
-    }
 
     return 0;
 }
@@ -39,14 +37,15 @@ int asrepl_engine_asm(asrepl_engine* e, const char* input, char** out)
 
 int asrepl_engine_disasm(asrepl_engine* e, const unsigned char* input, size_t length, char** out)
 {
-    cs_insn *insn;
+    cs_insn* insn;
 
-    size_t count = cs_disasm(e->capstone, input, length+1, 0x1000, 0, &insn);
+    size_t count = cs_disasm(e->capstone, input, length, 0x1000, 0, &insn);
     if (count > 0) {
-        char* result = malloc(sizeof(char) * 32);
+        char* result = malloc(sizeof(char) * 192 * count);
 
+        char* r = result;
         for (size_t i = 0; i < count; i++)
-            asprintf(&result, "%s %s\n", insn[i].mnemonic, insn[i].op_str);
+            r += sprintf(r, "%s %s\n", insn[i].mnemonic, insn[i].op_str);
 
         *out = result;
         cs_free(insn, count);
