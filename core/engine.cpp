fa13dc7de369c169bb63ff44da983497171d9e3e
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-12 18:20:48 -0400

    Add arch/mode-switching groundwork

--- core/engine.cpp
+++ core/engine.cpp
@@ -7,9 +7,35 @@
 namespace asrepl {
 
 engine::engine()
+    : m_arch(arch::intel)
+    , m_mode(mode::b64)
+    , m_active(false)
 {
-    ks_open(KS_ARCH_X86, KS_MODE_64, &m_ks);
-    cs_open(CS_ARCH_X86, CS_MODE_64, &m_cs);
+    restart();
+}
+
+void engine::restart()
+{
+    ks_arch ks_arch = KS_ARCH_X86;
+    int ks_mode = m_mode == mode::b64 ? KS_MODE_64 : KS_MODE_32;
+    cs_arch cs_arch = CS_ARCH_X86;
+    cs_mode cs_mode = m_mode == mode::b64 ? CS_MODE_64 : CS_MODE_32;
+
+    if (m_arch == arch::arm) {
+        ks_arch = m_mode == mode::b64 ? KS_ARCH_ARM64 : KS_ARCH_ARM;
+        ks_mode = KS_MODE_ARM;
+        cs_arch = m_mode == mode::b64 ? CS_ARCH_ARM64 : CS_ARCH_ARM;
+        cs_mode = CS_MODE_ARM;
+    }
+
+    if (m_active) {
+        ks_close(m_ks);
+        cs_close(&m_cs);
+    }
+
+    ks_open(ks_arch, ks_mode, &m_ks);
+    cs_open(cs_arch, cs_mode, &m_cs);
+    m_active = true;
 }
 
 std::string engine::assemble(const std::string& input)
@@ -46,6 +72,26 @@ std::string engine::disassemble(const std::vector<unsigned char>& input) const
     }
 
     return "";
+}
+
+enum engine::arch engine::arch() const
+{
+    return m_arch;
+}
+
+void engine::set_arch(enum arch a)
+{
+    m_arch = a;
+}
+
+enum engine::mode engine::mode() const
+{
+    return m_mode;
+}
+
+void engine::set_mode(enum mode m)
+{
+    m_mode = m;
 }
 
 }
--- core/engine.h
+++ core/engine.h
@@ -8,14 +8,36 @@
 namespace asrepl {
 
 class engine {
-    ks_engine* m_ks;
-    csh m_cs;
-
 public:
+    enum class arch {
+        intel,
+        arm
+    };
+
+    enum class mode {
+        b32,
+        b64
+    };
+
     engine();
 
+    void restart();
     std::string assemble(const std::string& input);
     std::string disassemble(const std::vector<unsigned char>& input) const;
+
+    arch arch() const;
+    void set_arch(enum arch arch);
+
+    mode mode() const;
+    void set_mode(enum mode mode);
+
+private:
+    enum arch m_arch;
+    enum mode m_mode;
+
+    bool m_active;
+    ks_engine* m_ks;
+    csh m_cs;
 };
 
 }
