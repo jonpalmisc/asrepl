471c4c0909f911069ed86954b9eb073676829039
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-12 17:52:40 -0400

    Some `clang-format`-ing

--- core/util.h
+++ core/util.h
@@ -4,9 +4,9 @@
 
 namespace asrepl::util {
 
-    bool is_hex_string(std::string input);
-    bool is_command_string(std::string input);
-    std::vector<std::string> tokenize(const std::string& input);
-    std::vector<unsigned char> hex_decode(const std::string& input);
+bool is_hex_string(std::string input);
+bool is_command_string(std::string input);
+std::vector<std::string> tokenize(const std::string& input);
+std::vector<unsigned char> hex_decode(const std::string& input);
 
 }
