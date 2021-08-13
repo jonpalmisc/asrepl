b19a4b8188df807dbd55e8ee1ca22b0e6b2adf52
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-12 20:56:27 -0400

    Suppress CMake warnings in web build script

--- buildweb.sh
+++ buildweb.sh
@@ -1,6 +1,6 @@
 rm -fr build
 
-emcmake cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=MinSizeRel -GNinja -DASREPL_CLI=OFF -DASREPL_WEB=ON
+emcmake cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=MinSizeRel -GNinja -DASREPL_CLI=OFF -DASREPL_WEB=ON -Wno-dev
 cd build
 ninja
 
