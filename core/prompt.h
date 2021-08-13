728b97362f8f99388ec86cc0c9fdd29d138c5cd7
Jon Palmisciano <jp@jonpalmisc.com>
2021-08-13 09:42:54 -0400

    Add more documentation to `prompt` class

--- core/prompt.h
+++ core/prompt.h
@@ -5,11 +5,21 @@
 
 namespace asrepl {
 
+/**
+ * A basic, generic prompt interface for use from the CLI or web app.
+ */
 class prompt {
     engine m_engine;
     bool m_exit_requested;
 
+    /**
+     * Handle an internal "slash command".
+     */
     std::string handle_command(const std::vector<std::string>& args);
+
+    /**
+     * Get the current architecture and mode string.
+     */
     std::string info_string() const;
 
 public:
@@ -20,7 +30,14 @@ public:
      */
     std::string welcome_message() const;
 
+    /**
+     * Send input to the prompt. Can be internal commands, mnemonics, or opcodes.
+     */
     std::string send(const std::string& input);
+
+    /**
+     * Was an exit requested?
+     */
     bool exit_requested() const;
 };
 
